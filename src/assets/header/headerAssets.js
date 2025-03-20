56bbff8772dc974d3fa210e6084be3c7e0134a3b
MohammedAbdBarakat <princeofyabroud77@gmail.com>
2025-03-20 17:59:21 +0300

    Initial commit: Upload Rawi project

---
+++ src/assets/header/headerAssets.js
@@ -0,0 +1,10 @@
+import cartIcon from "../header/cartIcon.png"
+import cartIcon1 from "../header/cartIcon1.png"
+import heartIcon from "../header/heartIcon.png"
+import searchIcon from "../header/searchIcon.png"
+
+const headerAssets = {
+    cartIcon, heartIcon, searchIcon, cartIcon1
+}
+
+export default headerAssets
